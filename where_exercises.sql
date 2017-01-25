7ab540a12890b7ee6304a0b52d09672e8669330c
V-Rod <pjvelazquez@me.com>
2017-01-25 11:53:33 -0600

    MySQL -Exercise 6.8.1 (where_exercises) Find all employees hired in the 90s and born on Christmas

--- where_exercises.sql
+++ where_exercises.sql
@@ -10,7 +10,8 @@ AND last_name like '%e';
 
 SELECT first_name, last_name
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND (birth_date LIKE '%12-25%' );
 
 SELECT first_name, last_name
 FROM employees
