e76564b2ab2c98c7755de15e0fa947839c3570ce
V-Rod <pjvelazquez@me.com>
2017-01-25 14:06:07 -0600

    MySQL -Exercise 6.8.3 (order by exersize) Now reverse the sort order for both queries.

--- sql/order_by_exercises.sql
+++ sql/order_by_exercises.sql
@@ -3,13 +3,13 @@ WHERE gender = 'M'
       AND (first_name = 'Irena'
            OR first_name= 'Vidya'
            OR first_name ='Maya')
-ORDER BY last_name ASC, first_name ASC;
+ORDER BY last_name DESC, first_name DESC;
 
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name LIKE 'e%'
       AND last_name like '%e'
-ORDER BY emp_no ASC;
+ORDER BY emp_no DESC;
 
 SELECT first_name, last_name
 FROM employees
