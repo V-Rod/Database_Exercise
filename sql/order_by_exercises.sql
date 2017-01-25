ed6d1f3e6b428cf0c4dc44338c89bc9007b7e866
V-Rod <pjvelazquez@me.com>
2017-01-25 14:00:02 -0600

    MySQL -Exercise 6.8.3 (order by exersize) Modify your first query to order by first name.

---
+++ sql/order_by_exercises.sql
@@ -0,0 +1,22 @@
+SELECT * FROM employees
+WHERE gender = 'M'
+      AND (first_name = 'Irena'
+           OR first_name= 'Vidya'
+           OR first_name ='Maya')
+ORDER BY first_name ASC;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'e%'
+      AND last_name like '%e';
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+      AND (birth_date LIKE '%12-25%');
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+      AND NOT last_name LIKE '%qu%';
+
