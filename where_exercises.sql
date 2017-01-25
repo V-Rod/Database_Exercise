836e45d0824e6029ad3d09e70417657c1129085d
V-Rod <pjvelazquez@me.com>
2017-01-25 11:16:14 -0600

    MySQL -Exercise 6.8.1 (where_exercises) Employees hired in the 90s

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,3 +4,8 @@ WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 SELECT last_name
 FROM employees
 WHERE last_name LIKE 'e%';
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
