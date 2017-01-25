044388127f4f7230feb3a1646a14f37e0621b795
V-Rod <pjvelazquez@me.com>
2017-01-25 11:26:00 -0600

    MySQL -Exercise 6.8.1 (where_exercises) Employees born on Christmas

--- where_exercises.sql
+++ where_exercises.sql
@@ -9,7 +9,9 @@ SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
-
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%12-25%';
 
 SELECT first_name, last_name
 FROM employees
