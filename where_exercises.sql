66f7156fb27823790efa911871463f3c14477062
V-Rod <pjvelazquez@me.com>
2017-01-25 11:29:08 -0600

    MySQL -Exercise 6.8.1 (where_exercises) refactored code

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,11 +1,11 @@
 SELECT * FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
-SELECT last_name
+SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'e%';
 
-SELECT *
+SELECT first_name, last_name
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
